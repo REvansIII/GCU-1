3cb2e52d031fc59904eb101a20236c515cb06c5f
REvansIII <hairbyrobbie@yahoo.com>
2016-08-31 11:03:58 -0700

    revision to NFLPlayer.java

---
+++ NFLPlayer.java
@@ -0,0 +1,63 @@
+package draft;
+
+//Milestone Assignment 2
+public class NFLPlayer {
+	String name;
+	int number;
+	String height;
+	int weight;
+	String position;
+	double yards;
+	int touchdowns;
+	String college;
+	double fortyyardtime;
+	double verticaljump;
+
+	public static void main(String[] args) {
+
+	}
+
+	// assign constructor to player class
+	public NFLPlayer(String name, int number) {
+
+	}
+
+	// height for player class
+	public void Height(String height) {
+
+	}
+
+	// set player height
+	public void Weight(int weight) {
+
+	}
+
+	// set player position
+	public void Position(String position) {
+
+	}
+
+	// accumulated yards
+	public void Yards(double yards) {
+
+	}
+
+	// player touchdowns
+	public void Touchdowns(int touchdowns) {
+	}
+
+	// player college attended
+	public void College() {
+
+	}
+
+	// player 40 yard dash time
+	public void FortyYardTime(double fortyyardtime) {
+
+	}
+
+	// player vertical jump
+	public void VerticalJump(double verticaljump) {
+
+	}
+}
